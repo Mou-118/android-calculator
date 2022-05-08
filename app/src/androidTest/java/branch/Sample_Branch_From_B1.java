076cf1deaf4d1ebe776d83a9f7481dd3f8c22336
Mou <mou1807118@stud.kuet.ac.bd>
2022-05-08 22:21:30 +0600

    MODIFIED SAMPLE BRANCH B1 FROM B2

--- app/src/androidTest/java/branch/Sample_Branch_From_B1.java
+++ app/src/androidTest/java/branch/Sample_Branch_From_B1.java
@@ -3,12 +3,13 @@ package branch;
 public class Sample_Branch_From_B1 {
     String name, id;
     float cgpa;
+    double income;
 
-    public Sample_Branch_From_B1(String s, String s1 , float f) {
-
-        this.name = s;
-        this.id=s1;
-        this.cgpa=f;
+    public Sample_Branch_From_B1(String name , String id, float cgpa,double income) {
+        this.name = name;
+        this.id=id;
+        this.cgpa=cgpa;
+        this.income=income;
     }
 
     public String getName() {
@@ -33,5 +34,13 @@ public class Sample_Branch_From_B1 {
 
     public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
     }
 }
